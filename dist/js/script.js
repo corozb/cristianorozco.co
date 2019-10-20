1459fa6a5b92f747edfaf1976e50064ae11f0c06
Cristian Orozco <cristianorozco1984@gmail.com>
2019-10-20 17:43:40 -0500

    scroll efect

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,4 +15,56 @@ $menuBtn.addEventListener('click', close)
 $menuItems.forEach(item => item.addEventListener('click', close))
 
 
+// scroll event
+function scrollMenu() {
+  // DOM Elements
+  const $home = document.querySelector('.home');
+  const $projects = document.getElementById('projects')
+  const $about = document.getElementById('about');
+  const $contact = document.getElementById('contact');
+  const $social = document.getElementById('social')
+  const $name = document.getElementById('name')
+  
+  // Menu Items
+  const $menuHome = document.querySelector('.homeItem');
+  const $menuProject = document.querySelector('.projectItem');
+  const $menuAbout = document.querySelector('.aboutItem');
+  const $menuContact = document.querySelector('.contactItem');
 
+  // Positions
+  const hPos = $home.getBoundingClientRect().top 
+  const prPos = $projects.getBoundingClientRect().top
+  const aPos = $about.getBoundingClientRect().top
+  const cPos = $contact.getBoundingClientRect().top
+
+
+  const screemHeight = window.innerHeight/5 
+
+    if (hPos < screemHeight || screemHeight == ' ') {
+      $menuHome.classList.add('selected')
+      $menuProject.classList.remove('selected')
+    }
+    if (prPos < screemHeight) {
+      $menuHome.classList.remove('selected')
+      $menuProject.classList.add('selected')
+      $menuAbout.classList.remove('selected')
+    }
+    if (aPos < screemHeight) {
+      $menuAbout.classList.add('selected')
+      $menuProject.classList.remove('selected')
+      $menuContact.classList.remove('selected')
+    }
+    if (cPos < screemHeight) {
+      $menuAbout.classList.remove('selected')
+      $menuContact.classList.add('selected')
+      $social.style.display = "none"
+      $name.style.display = "none"
+    }
+    else {
+      $social.style.display = "flex"
+      $name.style.display = "flex"
+    }
+}
+
+
+window.addEventListener('scroll', scrollMenu)
